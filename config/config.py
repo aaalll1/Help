d83364d3b9e1ae7d15a52ae6862671fd4c390558
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-05-31 14:27:53 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -240,7 +240,7 @@ PHOTO = list(
 
 START_IMG_URL = getenv(
     "START_IMG_URL",
-    "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg",
+    "https://graph.org/file/3cf49673f408204004418.jpg",
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
