df20b08f8186aab5b2818376e8f26f3ebb09b381
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-06-01 15:19:21 +0300

    Update keyboard.py

--- YukkiMusic/plugins/play/keyboard.py
+++ YukkiMusic/plugins/play/keyboard.py
@@ -2,7 +2,7 @@ from YukkiMusic import app
 from strings.filters import command
 from pyrogram import Client, filters
 from pyrogram.types import ReplyKeyboardMarkup, Message
-from config import OWNER, SUDO
+from config import OWNER
 
 # تحديد لوحة المفاتيح
 keyboard_main = ReplyKeyboardMarkup(
@@ -24,7 +24,7 @@ keyboard_main = ReplyKeyboardMarkup(
 
 keyboard_remove = ReplyKeyboardMarkup(
     [
-        [('⦗ اغلاق الكيبورد ⦘')],
+        [('⦗ فتح الكيبورد ⦘')],
     ],
     resize_keyboard=True,
     one_time_keyboard=False
@@ -32,10 +32,8 @@ keyboard_remove = ReplyKeyboardMarkup(
 
 @app.on_message(command(["⦗ فتح الكيبورد ⦘", "تنصيب الكيبورد"]) & filters.private & filters.user(OWNER))
 async def start_or_help_command(client, message: Message):
-    if message.from_user.id in SUDO or message.from_user.id == OWNER:
-        await message.reply_text('اهلأ بك عزيزي ⦗ المطور الاساسي ⦘ \n – – – – – – \n⦗ يمكنك التحكم عن طريق الأزرار أدناه ⦘', reply_markup=keyboard_main)
-    
+    await message.reply_text('اهلأ بك عزيزي ⦗ المطور الاساسي ⦘ \n – – – – – – \n⦗ يمكنك التحكم عن طريق الأزرار أدناه ⦘', reply_markup=keyboard_main)
+
 @app.on_message(command(["⦗ حذف الكيبورد ⦘"]) & filters.private & filters.user(OWNER))
 async def remove_keyboard(client, message: Message):
-    if message.from_user.id in SUDO or message.from_user.id == OWNER:
-        await message.reply_text('اهلأ بك عزيزي ⦗ المطور الاساسي ⦘ \n– – – – – – \n⦗ تم تنفيذ أمر لوحة التحكم ⦘', reply_markup=keyboard_remove)
+    await message.reply_text('اهلأ بك عزيزي ⦗ المطور الاساسي ⦘ \n– – – – – – \n⦗ تم تنفيذ أمر لوحة التحكم ⦘', reply_markup=keyboard_remove)
