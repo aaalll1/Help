bfda36753f07288963e894da1b6b5b691257555d
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-06-03 18:14:56 +0300

    Update songs.py

--- YukkiMusic/plugins/tools/songs.py
+++ YukkiMusic/plugins/tools/songs.py
@@ -2,6 +2,7 @@ import os
 import re
 import requests
 import yt_dlp
+from strings.filters import command
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from youtube_search import YoutubeSearch
@@ -10,6 +11,7 @@ from config import SUPPORT_CHANNEL, Muntazer
 from pyrogram.errors import UserNotParticipant, ChatAdminRequired, ChatWriteForbidden
 
 
+# دالة للتحقق من اشتراك المستخدم في القناة
 async def must_join_channel(app, msg):
     if not Muntazer:
         return
@@ -30,22 +32,22 @@ async def must_join_channel(app, msg):
                     f"~︙عليك الأشتراك في قناة البوت \n~︙قناة البوت : @{Muntazer}.",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("قناة الاشتراك", url=link)]
+                        [InlineKeyboardButton("⦗ قناة البوت ⦘", url=link)]
                     ])
                 )
                 await msg.stop_propagation()
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"I'm not admin in the MUST_JOIN chat {Muntazer}!")
+        print(f"I m not admin in the MUST_JOIN chat {Muntazer}!")
 
 
 def is_valid_youtube_url(url):
     # Check if the provided URL is a valid YouTube URL
-    return re.match(r'(https?://)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/', url)
+    return url.startswith(("https://www.youtube.com", "http://www.youtube.com", "youtube.com"))
 
 
-@app.on_message(filters.command(["يوت", "yt", "تنزيل", "بحث"]))
+@app.on_message(command(["يوت", "yt", "تنزيل", "بحث"]))
 async def song(_, message: Message):
     try:
         await message.delete()
@@ -55,7 +57,7 @@ async def song(_, message: Message):
     # تحقق من الاشتراك الإجباري
     await must_join_channel(app, message)
 
-    m = await message.reply_text("- يتم البحث الان .", quote=True)
+    m = await message.reply_text("⦗ جارِ البحث يرجى الانتضار ⦘", quote=True)
 
     query = " ".join(str(i) for i in message.command[1:])
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
@@ -72,35 +74,20 @@ async def song(_, message: Message):
             
             link = f"https://youtube.com{results[0]['url_suffix']}"
 
-        if is_valid_youtube_url(link):
-            # Check if the link is a video or audio
-            if "/watch?v=" in link:
-                search_type = "video"
-            else:
-                search_type = "audio"
-
-            if search_type == "video" and "بحث" in query:
-                ydl_opts = {"format": "bestaudio[ext=m4a]"}
-                search_type = "video"
-            else:
-                ydl_opts = {"format": "bestaudio[ext=m4a]"}
-
-            # Extract information if it's a search query
-            if not is_valid_youtube_url(query):
-                title = results[0]["title"][:40]
-                thumbnail = results[0]["thumbnails"][0]
-                thumb_name = f"{title}.jpg"
-                # Replace invalid characters in the filename
-                thumb_name = thumb_name.replace("/", "")
-                thumb = requests.get(thumbnail, allow_redirects=True)
-                open(thumb_name, "wb").write(thumb.content)
-                duration = results[0]["duration"]
+        title = results[0]["title"][:40]
+        thumbnail = results[0]["thumbnails"][0]
+        thumb_name = f"{title}.jpg"
+        # Replace invalid characters in the filename
+        thumb_name = thumb_name.replace("/", "")
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, "wb").write(thumb.content)
+        duration = results[0]["duration"]
 
     except Exception as ex:
         error_message = f"- فشل .\n\n**السبب :** `{ex}`"
         return await m.edit_text(error_message)
 
-    await m.edit_text("- تم الرفع انتضر قليلاً .")
+    await m.edit_text("⦗ جارِ التحميل، يرجى الانتظار قليلاً ... ⦘")
     audio_file = ''
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -108,75 +95,47 @@ async def song(_, message: Message):
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
 
-        if search_type == "video":
-            msg = await message.reply("- يتم البحث الان .")
-            try:
-                with yt_dlp.YoutubeDL(ydl_opts) as ytdl:
-                    ytdl_data = ytdl.extract_info(link, download=True)
-                    file_name = ytdl.prepare_filename(ytdl_data)
-            except Exception as e:
-                return await msg.edit(f"🚫 **error:** {e}")
+        rep = f"**• by :** {message.from_user.first_name}"
 
-            thumb_path = f"thumb{title}.jpg"
-            if not os.path.exists(thumb_path):
-                return await msg.edit(f"🚫 **error:** Thumb file not found!")
+        secmul, dur, dur_arr = 1, 0, duration.split(":")
+        for i in range(len(dur_arr) - 1, -1, -1):
+            dur += int(dur_arr[i]) * secmul
+            secmul *= 60
 
-            await msg.edit("- تم الرفع انتضر قليلاً .")
-            await message.reply_video(
-                file_name,
-                duration=int(ytdl_data["duration"]),
-                thumb=thumb_path,
-                caption=ytdl_data["title"],
-            )
-            try:
-                os.remove(file_name)
-                os.remove(thumb_path)
-                await msg.delete()
-            except Exception as ex:
-                print(f"- فشل : {ex}")
+        visit_butt = InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton(text="⦗ Источник ⦘", url=SUPPORT_CHANNEL)],
+            ]
+        )
+        # Reply to the user who initiated the search
+        await message.reply_audio(
+            audio=audio_file,
+            caption=rep,
+            thumb=thumb_name,
+            title=title,
+            duration=dur,
+            reply_markup=visit_butt,
+        )
 
-        elif search_type == "audio":
-            rep = f"**- الأسم :** [{title[:23]}]({link})\n**- الوقت :** `{duration}`\n**- بواسطة  :** {message.from_user.first_name}"
-
-            secmul, dur, dur_arr = 1, 0, duration.split(":")
-            for i in range(len(dur_arr) - 1, -1, -1):
-                dur += int(dur_arr[i]) * secmul
-                secmul *= 60
-
-            visit_butt = InlineKeyboardMarkup(
-                [
-                    [InlineKeyboardButton(text="- المنشئ .", url=SUPPORT_CHANNEL)],
-                ]
-            )
-            # الرد على المستخدم الذي بدأ البحث
-            await message.reply_audio(
-                audio=audio_file,
-                caption=rep,
-                thumb=thumb_name,
-                title=title,
-                duration=dur,
-                reply_markup=visit_butt,
-            )
+        await m.delete()
 
     except Exception as ex:
-        error_message = f"- فشل في تحميل الفيديو أو الصوت من YouTube. \n\n**السبب :** `{ex}`"
+        error_message = f"- فشل في تحميل الفيديو من YouTube. \n\n**السبب :** `{ex}`"
         await m.edit_text(error_message)
 
-    # إزالة الملفات المؤقتة بعد رفع الصوت أو الفيديو
+    # Remove temporary files after audio upload
     try:
         if audio_file:
             os.remove(audio_file)
-        if thumb_name:
-            os.remove(thumb_name)
+        os.remove(thumb_name)
     except Exception as ex:
         error_message = f"- فشل في حذف الملفات المؤقتة. \n\n**السبب :** `{ex}`"
         await m.edit_text(error_message)
 
 
-
-@app.on_message(filters.command(["تحميل", "video"]))
+@app.on_message(command(["تحميل", "video"]))
 async def video_search(client, message):
-    ydl_opts = {
+        ydl_opts = {
         "format": "best",
         "keepvideo": True,
         "prefer_ffmpeg": False,
@@ -186,9 +145,10 @@ async def video_search(client, message):
     }
     query = " ".join(message.command[1:])
     try:
-        # تحقق من الاشتراك الإجباري
+    
+       # تحقق من الاشتراك الإجباري
         await must_join_channel(app, message)
-
+  
         results = YoutubeSearch(query, max_results=1).to_dict()
         link = f"https://youtube.com{results[0]['url_suffix']}"
         title = results[0]["title"][:40]
@@ -199,11 +159,14 @@ async def video_search(client, message):
         thumb = requests.get(thumbnail, allow_redirects=True)
         with open(thumb_name, "wb") as file:
             file.write(thumb.content)
-
+        results[0]["duration"]
+        results[0]["url_suffix"]
+        results[0]["views"]
+        message.from_user.mention
     except Exception as e:
-        return print(f"🚫 **error:** {e}")
+        print(e)
     try:
-        msg = await message.reply("- يتم البحث الان .")
+        msg = await message.reply("⦗ جارِ البحث يرجى الانتضار ⦘")
         with yt_dlp.YoutubeDL(ydl_opts) as ytdl:
             ytdl_data = ytdl.extract_info(link, download=True)
             file_name = ytdl.prepare_filename(ytdl_data)
@@ -212,8 +175,8 @@ async def video_search(client, message):
     thumb_path = f"thumb{title}.jpg"
     if not os.path.exists(thumb_path):
         return await msg.edit(f"🚫 **error:** Thumb file not found!")
-
-    await msg.edit("- تم الرفع انتضر قليلاً .")
+    
+    await msg.edit("⦗ جارِ التحميل، يرجى الانتظار قليلاً ... ⦘")
     await message.reply_video(
         file_name,
         duration=int(ytdl_data["duration"]),
