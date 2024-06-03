2cf050e4d6c38adf6e5d7cc1c5f566c3cdd5adff
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-06-03 18:53:41 +0300

    Create link.py

---
+++ YukkiMusic/plugins/tools/link.py
@@ -0,0 +1,135 @@
+import os
+import requests
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from youtube_dl import YoutubeDL
+from youtube_search import YoutubeSearch
+from config import SUPPORT_CHANNEL, Muntazer
+from pyrogram.errors import UserNotParticipant, ChatWriteForbidden
+from strings.filters import command
+
+# دالة للتحقق من اشتراك المستخدم في القناة
+async def must_join_channel(app, msg):
+    if not Muntazer:
+        return
+    try:
+        if msg.from_user is None:
+            return
+        
+        try:
+            await app.get_chat_member(Muntazer, msg.from_user.id)
+        except UserNotParticipant:
+            if Muntazer.isalpha():
+                link = "https://t.me/" + Muntazer
+            else:
+                chat_info = await app.get_chat(Muntazer)
+                link = chat_info.invite_link
+            try:
+                await msg.reply(
+                    f"~︙عليك الأشتراك في قناة البوت \n~︙قناة البوت : @{Muntazer}.",
+                    disable_web_page_preview=True,
+                    reply_markup=InlineKeyboardMarkup([
+                        [InlineKeyboardButton("⦗ قناة البوت ⦘", url=link)]
+                    ])
+                )
+                await msg.stop_propagation()
+            except ChatWriteForbidden:
+                pass
+    except Exception as ex:
+        print(f"Error: {str(ex)}")
+
+
+def is_valid_youtube_url(url):
+    # Check if the provided URL is a valid YouTube URL
+    return url.startswith(("https://www.youtube.com", "http://www.youtube.com", "youtube.com"))
+
+
+@app.on_message(command(["يوتيوب", "رابط"]))
+async def song(_, message: Message):
+    try:
+        await message.delete()
+    except:
+        pass
+    
+    # تحقق من الاشتراك الإجباري
+    await must_join_channel(app, message)
+
+    m = await message.reply_text("⦗ جارِ البحث يرجى الانتضار ⦘", quote=True)
+
+    query = " ".join(str(i) for i in message.command[1:])
+
+    try:
+        if is_valid_youtube_url(query):
+            link = query
+        else:
+            results = YoutubeSearch(query, max_results=1).to_dict()
+            if not results:
+                raise Exception("- لايوجد بحث .")
+            
+            link = f"https://youtube.com{results[0]['url_suffix']}"
+
+        title = results[0]["title"][:40]
+        thumbnail = results[0]["thumbnails"][0]
+        thumb_name = f"{title}.jpg"
+        thumb_name = thumb_name.replace("/", "")
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, "wb").write(thumb.content)
+        duration = results[0]["duration"]
+
+    except Exception as ex:
+        error_message = f"- فشل .\n\n**السبب :** `{ex}`"
+        return await m.edit_text(error_message)
+
+    await m.edit_text("⦗ جارِ التحميل، يرجى الانتظار قليلاً ... ⦘")
+
+    try:
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': '%(title)s.%(ext)s',
+            'noplaylist': True,
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '320',
+            }],
+        }
+        with YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=True)
+            audio_file = ydl.prepare_filename(info_dict)
+        
+        rep = f"**• by :** {message.from_user.first_name}"
+
+        secmul, dur, dur_arr = 1, 0, duration.split(":")
+        for i in range(len(dur_arr) - 1, -1, -1):
+            dur += int(dur_arr[i]) * secmul
+            secmul *= 60
+
+        visit_butt = InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton(text="⦗ Источник ⦘", url=SUPPORT_CHANNEL)],
+            ]
+        )
+
+        await message.reply_audio(
+            audio=audio_file,
+            caption=rep,
+            thumb=thumb_name,
+            title=title,
+            duration=dur,
+            reply_markup=visit_butt,
+        )
+
+        await m.delete()
+
+    except Exception as ex:
+        error_message = f"- فشل في تحميل الفيديو من YouTube. \n\n**السبب :** `{ex}`"
+        await m.edit_text(error_message)
+
+    # Remove temporary files after audio upload
+    try:
+        if audio_file:
+            os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as ex:
+        error_message = f"- فشل في حذف الملفات المؤقتة. \n\n**السبب :** `{ex}`"
+        await m.edit_text(error_message)
