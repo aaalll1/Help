dbbfa4da5e15ab54bf20ade6e22b140e2de19db1
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-06-03 23:02:13 +0300

    Update songs.py

--- YukkiMusic/plugins/tools/songs.py
+++ YukkiMusic/plugins/tools/songs.py
@@ -90,7 +90,7 @@ async def song(_, message: Message):
                 audio_file = ydl.prepare_filename(info_dict)
                 ydl.process_info(info_dict)
 
-            rep = f"**• by :** {message.from_user.first_name if message.from_user else 'Unknown User'} \n⎯ ⎯ ⎯ ⎯\n• ch : @{Muntazer} ."
+            rep = f"**• by :** {message.from_user.first_name if message.from_user else 'Freedom'} \n⎯ ⎯ ⎯ ⎯\n• ch : @{Muntazer} ."
 
             secmul, dur, dur_arr = 1, 0, duration.split(":")
             for i in range(len(dur_arr) - 1, -1, -1):
