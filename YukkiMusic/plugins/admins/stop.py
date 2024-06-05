400ddf8edbd0028e4e6d4c9bcca52dc2c0810550
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-06-05 18:43:24 +0300

    Update stop.py

--- YukkiMusic/plugins/admins/stop.py
+++ YukkiMusic/plugins/admins/stop.py
@@ -10,22 +10,50 @@
 
 from pyrogram import filters
 from pyrogram.types import Message
-
+from strings.filters import command
 from config import BANNED_USERS
-from strings import get_command
 from YukkiMusic import app
+from config import Muntazer
 from YukkiMusic.core.call import Yukki
 from YukkiMusic.utils.database import set_loop
 from YukkiMusic.utils.decorators import AdminRightsCheck
 
-# Commands
-STOP_COMMAND = get_command("STOP_COMMAND")
+@app.on_message(filters.incoming & filters.private, group=-1) 
+async def must_join_channel(app, msg):
+    if not Muntazer:
+        return
+    try:
+        if msg.from_user is None:
+            return
+        try:
+            await app.get_chat_member(Muntazer, msg.from_user.id)
+        except UserNotParticipant:
+            if Muntazer.isalpha():
+                link = "https://t.me/" + Muntazer
+            else:
+                chat_info = await app.get_chat(Muntazer)
+                link = chat_info.invite_link
+            try:
+                await msg.reply(
+                    f"~︙عليك الأشتراك في قناة البوت \n~︙قناة البوت : @{Muntazer}.",
+                    disable_web_page_preview=True,
+                    reply_markup=InlineKeyboardMarkup([
+                        [InlineKeyboardButton("⦗ قناة الإشتراك ⦘", url=link)]
+                    ])
+                )
+                await msg.stop_propagation()
+            except ChatWriteForbidden:
+                pass
+    except ChatAdminRequired:
+        print(f"I m not admin in the MUST_JOIN chat {Muntazer}!")
 
 
-@app.on_message(filters.command(STOP_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(command(["ايقاف","انهاء","اوكف"]) & ~BANNED_USERS)
 @AdminRightsCheck
 async def stop_music(cli, message: Message, _, chat_id):
-    if not len(message.command) == 1:
+        if not len(message.command) == 1: 
+        return
+        await must_join_channel(cli, message) 
         return await message.reply_text(_["general_2"])
     await Yukki.stop_stream(chat_id)
     await set_loop(chat_id, 0)
