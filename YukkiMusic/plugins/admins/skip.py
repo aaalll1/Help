b8cdcdf00091717d697a7c16901d4092804c753d
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-06-05 22:37:07 +0300

    Update skip.py

--- YukkiMusic/plugins/admins/skip.py
+++ YukkiMusic/plugins/admins/skip.py
@@ -14,231 +14,199 @@ from YukkiMusic.utils.stream.autoclear import auto_clean
 from YukkiMusic.utils.thumbnails import gen_thumb
 
 
-@app.on_message(command(["سكب", "تخطي", "التالي"]) & ~BANNED_USERS)
+@app.on_message(command(["تخطي", "cnext"]) & ~BANNED_USERS)
 @AdminRightsCheck
-async def skip(cli, message: Message):
+async def skip_command(cli, message: Message):
     chat_id = message.chat.id
-    
-    if not len(message.command) < 2:
+    if not Muntazer:
+        return
+    try:
+        if message.from_user is None:
+            return
+
+        await cli.get_chat_member(Muntazer, message.from_user.id)
+    except UserNotParticipant:
+        if Muntazer.isalpha():
+            link = "https://t.me/" + Muntazer
+        else:
+            chat_info = await cli.get_chat(Muntazer)
+            link = chat_info.invite_link
+        await message.reply(
+            f"~︙عزيزي {message.from_user.mention} \n~︙عليك الأشتراك في قناة البوت \n~︙قناة البوت : @{Muntazer}.",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton("< Source Plus >", url=link)]
+            ])
+        )
+        return
+
+    try:
         loop = await get_loop(chat_id)
         if loop != 0:
-            return await message.reply_text(_["admin_12"])
-        
-        # Check if user is subscribed to the channel
-        try:
-            await app.get_chat_member(Muntazer, message.from_user.id)
-        except UserNotParticipant:
-            # User is not subscribed, send message to subscribe
-            if Muntazer.isalpha():
-                link = "https://t.me/" + Muntazer
-            else:
-                chat_info = await app.get_chat(Muntazer)
-                link = chat_info.invite_link
-            try:
-                await message.reply(
-                    f"عليك الأشتراك في قناة البوت لاستخدام هذا الأمر.\n"
-                    f"قناة البوت: @{Muntazer}.",
-                    disable_web_page_preview=True,
-                    reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("⦗قناة الإشتراك⦘", url=link)]
-                    ])
-                )
-            except ChatWriteForbidden:
-                pass
-            return
-        except ChatAdminRequired:
-            print(f"I'm not admin in the MUST_JOIN chat {Muntazer}!")
-            return
-        
-        state = message.text.split(None, 1)[1].strip()
-        if state.isnumeric():
-            state = int(state)
-            check = db.get(chat_id)
-            if check:
-                count = len(check)
-                if count > 2:
-                    count = int(count - 1)
-                    if 1 <= state <= count:
-                        for x in range(state):
-                            popped = None
-                            try:
-                                popped = check.pop(0)
-                            except:
-                                return await message.reply_text(_["admin_16"])
-                            if popped:
-                                await auto_clean(popped)
-                            if not check:
-                                try:
-                                    await message.reply_text(
-                                        _["admin_10"].format(
-                                            message.from_user.first_name
-                                        ),
-                                        disable_web_page_preview=True,
-                                    )
-                                    await Yukki.stop_stream(chat_id)
-                                except:
-                                    return
-                                break
-                    else:
-                        return await message.reply_text(_["admin_15"].format(count))
-                else:
-                    return await message.reply_text(_["admin_14"])
-            else:
-                return await message.reply_text(_["queue_2"])
-        else:
-            return await message.reply_text(_["admin_13"])
-    else:
+            return await message.reply_text(_["admin_8"])
+
         check = db.get(chat_id)
-        popped = None
-        try:
-            popped = check.pop(0)
-            if popped:
-                await auto_clean(popped)
-            if not check:
-                await message.reply_text(
-                    _["admin_10"].format(message.from_user.first_name),
-                    disable_web_page_preview=True,
-                )
-                try:
-                    return await Yukki.stop_stream(chat_id)
-                except:
-                    return
-        except:
+        if not check:
+            return await message.reply_text(_["queue_2"])
+
+        popped = check.pop(0)
+        if popped:
+            await auto_clean(popped)
+        if not check:
+            await message.reply_text(
+                text=_["admin_6"].format(
+                    message.from_user.mention, message.chat.title
+                ),
+                reply_markup=close_markup(_),
+            )
             try:
-                await message.reply_text(
-                    _["admin_10"].format(message.from_user.first_name),
-                    disable_web_page_preview=True,
-                )
                 return await Yukki.stop_stream(chat_id)
             except:
                 return
-    
-    queued = check[0]["file"]
-    title = (check[0]["title"]).title()
-    user = check[0]["by"]
-    streamtype = check[0]["streamtype"]
-    videoid = check[0]["vidid"]
-    duration_min = check[0]["dur"]
-    status = True if str(streamtype) == "video" else None
-    
-    if "live_" in queued:
-        n, link = await YouTube.video(videoid, True)
-        if n == 0:
-            return await message.reply_text(_["admin_11"].format(title))
-        try:
-            await Yukki.skip_stream(chat_id, link, video=status)
-        except Exception as e:
-            print(e)  # Print the exception for debugging purposes
-            return await message.reply_text(_["call_9"])
-        button = telegram_markup(_, chat_id)
-        img = await gen_thumb(videoid)
-        run = await message.reply_photo(
-            photo=img,
-            caption=_["stream_1"].format(
-                user,
-                f"https://t.me/xl444",
-            ),
-            reply_markup=InlineKeyboardMarkup(button),
-        )
-        db[chat_id][0]["mystic"] = run
-        db[chat_id][0]["markup"] = "tg"
-        
-    elif "vid_" in queued:
-        mystic = await message.reply_text(_["call_10"], disable_web_page_preview=True)
-        try:
-            file_path, direct = await YouTube.download(
-                videoid,
-                mystic,
-                videoid=True,
-                video=status,
-            )
-        except Exception as e:
-            print(e)  # Print the exception for debugging purposes
-            return await mystic.edit_text(_["call_9"])
-        try:
-            await Yukki.skip_stream(chat_id, file_path, video=status)
-        except Exception as e:
-            print(e)  # Print the exception for debugging purposes
-            return await mystic.edit_text(_["call_9"])
-        button = stream_markup(_, videoid, chat_id)
-        img = await gen_thumb(videoid)
-        run = await message.reply_photo(
-            photo=img,
-            caption=_["stream_1"].format(
-                title[:27],
-                f"https://t.me/xl444",
-                duration_min,
-                user,
-            ),
-            reply_markup=InlineKeyboardMarkup(button),
-        )
-        db[chat_id][0]["mystic"] = run
-        db[chat_id][0]["markup"] = "stream"
-        await mystic.delete()
-    
-    elif "index_" in queued:
-        try:
-            await Yukki.skip_stream(chat_id, videoid, video=status)
-        except Exception as e:
-            print(e)  # Print the exception for debugging purposes
-            return await message.reply_text(_["call_9"])
-        button = telegram_markup(_, chat_id)
-        run = await message.reply_photo(
-            photo=config.STREAM_IMG_URL,
-            caption=_["stream_2"].format(user),
-            reply_markup=InlineKeyboardMarkup(button),
-        )
-        db[chat_id][0]["mystic"] = run
-        db[chat_id][0]["markup"] = "tg"
-    
-    else:
-        try:
-            await Yukki.skip_stream(chat_id, queued, video=status)
-        except Exception as e:
-            print(e)  # Print the exception for debugging purposes
-            return await message.reply_text(_["call_9"])
-        
-        if videoid == "telegram":
-            button = telegram_markup(_, chat_id)
-            run = await message.reply_photo(
-                photo=(
-                    config.TELEGRAM_AUDIO_URL
-                    if str(streamtype) == "audio"
-                    else config.TELEGRAM_VIDEO_URL
-                ),
-                caption=_["stream_3"].format(title, check[0]["dur"], user),
-                reply_markup=InlineKeyboardMarkup(button),
-            )
-            db[chat_id][0]["mystic"] = run
-            db[chat_id][0]["markup"] = "tg"
-        
-        
-        elif videoid == "soundcloud":
-            button = telegram_markup(_, chat_id)
-            run = await message.reply_photo(
-                photo=(
-                    config.SOUNCLOUD_IMG_URL
-                    if str(streamtype) == "audio"
-                    else config.TELEGRAM_VIDEO_URL
-                ),
-                caption=_["stream_3"].format(title, check[0]["dur"], user),
-                reply_markup=InlineKeyboardMarkup(button),
-            )
-            db[chat_id][0]["mystic"] = run
-            db[chat_id][0]["markup"] = "tg"
-        
-        else:
-            button = stream_markup(_, videoid, chat_id)
-            img = await gen_thumb(videoid)
+
+        queued = check[0]["file"]
+        title = check[0]["title"].title()
+        user = check[0]["by"]
+        streamtype = check[0]["streamtype"]
+        videoid = check[0]["vidid"]
+        status = True if str(streamtype) == "video" else None
+
+        db[chat_id][0]["played"] = 0
+        exis = check[0].get("old_dur")
+        if exis:
+            db[chat_id][0]["dur"] = exis
+            db[chat_id][0]["seconds"] = check[0]["old_second"]
+            db[chat_id][0]["speed_path"] = None
+            db[chat_id][0]["speed"] = 1.0
+
+        if "live_" in queued:
+            n, link = await YouTube.video(videoid, True)
+            if n == 0:
+                return await message.reply_text(_["admin_7"].format(title))
+            try:
+                image = await YouTube.thumbnail(videoid, True)
+            except:
+                image = None
+            try:
+                await Yukki.skip_stream(chat_id, link, video=status, image=image)
+            except:
+                return await message.reply_text(_["call_6"])
+            button = stream_markup(_, chat_id)
+            img = await get_thumb(videoid)
             run = await message.reply_photo(
                 photo=img,
                 caption=_["stream_1"].format(
-                    title[:27],
-                    f"https://t.me/xl444",
-                    duration_min,
+                    f"https://t.me/{app.username}?start=info_{videoid}",
+                    title[:23],
+                    check[0]["dur"],
+                    user,
+                ),
+                reply_markup=InlineKeyboardMarkup(button),
+            )
+            db[chat_id][0]["mystic"] = run
+            db[chat_id][0]["markup"] = "tg"
+        elif "vid_" in queued:
+            mystic = await message.reply_text(_["call_7"], disable_web_page_preview=True)
+            try:
+                file_path, direct = await YouTube.download(
+                    videoid,
+                    mystic,
+                    videoid=True,
+                    video=status,
+                )
+            except:
+                return await mystic.edit_text(_["call_6"])
+            try:
+                image = await YouTube.thumbnail(videoid, True)
+            except:
+                image = None
+            try:
+                await Yukki.skip_stream(chat_id, file_path, video=status, image=image)
+            except:
+                return await mystic.edit_text(_["call_6"])
+            button = stream_markup(_, chat_id)
+            img = await get_thumb(videoid)
+            run = await message.reply_photo(
+                photo=img,
+                caption=_["stream_1"].format(
+                    f"https://t.me/{app.username}?start=info_{videoid}",
+                    title[:23],
+                    check[0]["dur"],
                     user,
                 ),
                 reply_markup=InlineKeyboardMarkup(button),
             )
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "stream"
+            await mystic.delete()
+        elif "index_" in queued:
+            try:
+                await Yukki.skip_stream(chat_id, videoid, video=status)
+            except:
+                return await message.reply_text(_["call_6"])
+            button = stream_markup(_, chat_id)
+            run = await message.reply_photo(
+                photo=config.STREAM_IMG_URL,
+                caption=_["stream_2"].format(user),
+                reply_markup=InlineKeyboardMarkup(button),
+            )
+            db[chat_id][0]["mystic"] = run
+            db[chat_id][0]["markup"] = "tg"
+        else:
+            if videoid == "telegram":
+                image = None
+            elif videoid == "soundcloud":
+                image = None
+            else:
+                try:
+                    image = await YouTube.thumbnail(videoid, True)
+                except:
+                    image = None
+            try:
+                await Yukki.skip_stream(chat_id, queued, video=status, image=image)
+            except:
+                return await message.reply_text(_["call_6"])
+            if videoid == "telegram":
+                button = stream_markup(_, chat_id)
+                run = await message.reply_photo(
+                    photo=config.TELEGRAM_AUDIO_URL
+                    if str(streamtype) == "audio"
+                    else config.TELEGRAM_VIDEO_URL,
+                    caption=_["stream_1"].format(
+                        config.SUPPORT_CHANNEL, title[:23], check[0]["dur"], user
+                    ),
+                    reply_markup=InlineKeyboardMarkup(button),
+                )
+                db[chat_id][0]["mystic"] = run
+                db[chat_id][0]["markup"] = "tg"
+            elif videoid == "soundcloud":
+                button = stream_markup(_, chat_id)
+                run = await message.reply_photo(
+                    photo=config.SOUNCLOUD_IMG_URL
+                    if str(streamtype) == "audio"
+                    else config.TELEGRAM_VIDEO_URL,
+                    caption=_["stream_1"].format(
+                        config.SUPPORT_CHANNEL, title[:23], check[0]["dur"], user
+                    ),
+                    reply_markup=InlineKeyboardMarkup(button),
+                )
+                db[chat_id][0]["mystic"] = run
+                db[chat_id][0]["markup"] = "tg"
+            else:
+                button = stream_markup(_, chat_id)
+                img = await get_thumb(videoid)
+                run = await message.reply_photo(
+                    photo=img,
+                    caption=_["stream_1"].format(
+                        f"https://t.me/{app.username}?start=info_{videoid}",
+                        title[:23],
+                        check[0]["dur"],
+                        user,
+                    ),
+                    reply_markup=InlineKeyboardMarkup(button),
+                )
+                db[chat_id][0]["mystic"] = run
+                db[chat_id][0]["markup"] = "stream"
+
+    except ChatAdminRequired:
+        print(f"I'm not admin in the MUST_JOIN chat {Muntazer}!")
