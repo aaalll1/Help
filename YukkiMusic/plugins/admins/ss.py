cf76331476614e5c48e146cfb120c2d0ee38517a
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-06-09 20:39:03 +0300

    Update ss.py

--- YukkiMusic/plugins/admins/ss.py
+++ YukkiMusic/plugins/admins/ss.py
@@ -116,7 +116,6 @@ def get_bot_speed():
 
 @app.on_message(command(["معلومات التشغيل", "السيرفر"]) & (filters.private | filters.group))
 async def fetch_system_information(client, message):
-    owner_ids = OWNER_ID if isinstance(OWNER_ID, list) else [OWNER_ID]
     if message.from_user.id in owner_ids:      
         keyboard = InlineKeyboardMarkup(
             [
@@ -164,7 +163,6 @@ async def fetch_system_information(client, message):
     
 @app.on_callback_query()
 async def callback_query_handler(client, query):
-    owner_ids = OWNER_ID if isinstance(OWNER_ID, list) else [OWNER_ID]
     if query.from_user.id not in owner_ids:
         await query.answer("لاتدوس ترى هذا الزر خاص بمطور البوت.", show_alert=True)
         return
