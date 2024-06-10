3f3c94244da95999690b927c5fd20ee15b8f3ce2
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-06-11 01:23:32 +0300

    Update saden.py

--- YukkiMusic/plugins/bot/saden.py
+++ YukkiMusic/plugins/bot/saden.py
@@ -3,34 +3,37 @@ from YukkiMusic import app
 import asyncio
 import config
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.types import VideoChatEnded, Message
 from pytgcalls import PyTgCalls, StreamType
-from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
+from pytgcalls.types import InputStream, AudioParameters, AudioVideoParameters
 from YukkiMusic.core.call import Yukki
 from YukkiMusic.utils.database import *
-from pytgcalls.exceptions import (NoActiveGroupCall,TelegramServerError,AlreadyJoinedError)
-
+from pytgcalls.exceptions import NoActiveGroupCall, TelegramServerError, AlreadyJoinedError
 
 @app.on_message(filters.regex("^الصاعدين$"))
 async def strcall(client, message):
     assistant = await group_assistant(Dil, message.chat.id)
     try:
-        await assistant.join_group_call(message.chat.id, AudioPiped("https://graph.org/file/217aac5f9cd2b05f7ba5a.mp4"), stream_type=StreamType().pulse_stream)
+        await assistant.join_group_call(
+            message.chat.id,
+            InputStream(
+                AudioPiped("https://graph.org/file/217aac5f9cd2b05f7ba5a.mp4"),
+                AudioParameters(
+                    bit_rate=64000
+                )
+            ),
+            stream_type=StreamType().pulse_stream
+        )
         text = "-› الصاعدين بالأتصال :\n\n"
         participants = await assistant.get_participants(message.chat.id)
         k = 0
         for participant in participants:
             info = participant
-            if info.muted == False:
-                mut = "-› جاي يسولف "
-            else:
-                mut = "-› ساد المايك "
+            mut = "-› جاي يسولف " if not info.muted else "-› ساد المايك "
             user = await client.get_users(participant.user_id)
             k += 1
             text += f"{k} -› {user.mention} {mut}\n"
-        text += f"\n-› عددهم : {len(participants)}\n️"  
+        text += f"\n-› عددهم : {len(participants)}\n️"
 
-        # إضافة زر شفاف في الأسفل
         inline_keyboard = InlineKeyboardMarkup([
             [InlineKeyboardButton("⦗ قناة التحديثات ⦘", url=config.SUPPORT_CHANNEL)],
         ])      
@@ -48,16 +51,12 @@ async def strcall(client, message):
         k = 0
         for participant in participants:
             info = participant
-            if info.muted == False:
-                mut = "-› جاي يسولف "
-            else:
-                mut = "-› ساد المايك "
+            mut = "-› جاي يسولف " if not info.muted else "-› ساد المايك "
             user = await client.get_users(participant.user_id)
             k += 1
             text += f"{k} ~ {user.mention} {mut}\n"
         text += f"\n-› عددهم : {len(participants)}\n️"
 
-        # إضافة زر شفاف في الأسفل
         inline_keyboard = InlineKeyboardMarkup([
             [InlineKeyboardButton("⦗ قناة التحديثات ⦘ ", url=config.SUPPORT_CHANNEL)],
         ])
