6e7dc8cc5dde6fa1e3c09cbb44d470508a96540d
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-06-10 17:06:21 +0300

    Update kin.py

--- YukkiMusic/plugins/bot/kin.py
+++ YukkiMusic/plugins/bot/kin.py
@@ -4,28 +4,20 @@ from YukkiMusic import app
 
 @app.on_message(filters.regex("^مطور السورس$"))
 def show_developer_info(client, message):
-    # Developer ID (Replace with actual developer ID)
     developer_id = "1854384004"
     user = app.get_users(developer_id)
 
-    if user.photo:
-        photo_file = app.download_media(user.photo.big_file_id)
-    else:
-        photo_file = None
-
     bio = getattr(user, "bio", "No Bio")
+    username = getattr(user, "username", "No Username")
 
-    caption = f"Name: {user.first_name}\nID: {user.id}\nBio: {bio}"
+    caption = f"Name: {user.first_name}\nID: {user.id}\nBio: {bio}\nUsername: @{username}"
 
-    # Send photo to the user who sent "مطور السورس"
-    if photo_file:
-        client.send_photo(
-            chat_id=message.from_user.id,
-            photo=photo_file,
-            caption=caption
-        )
-    else:
-        client.send_message(
-            chat_id=message.from_user.id,
-            text=caption
-        )
+    inline_keyboard = InlineKeyboardMarkup(
+        [[InlineKeyboardButton("⦗ مطور السورس ⦘", url=f"https://t.me/{username}")]]
+    )
+
+    client.send_message(
+        chat_id=message.chat.id, 
+        text=caption,
+        reply_markup=inline_keyboard  
+    )
