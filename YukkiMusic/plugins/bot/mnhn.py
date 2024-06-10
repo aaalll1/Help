f69a33eb7afb5b59277ac8d25b18d2c3b5294351
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-06-10 04:26:03 +0300

    Update mnhn.py

--- YukkiMusic/plugins/bot/mnhn.py
+++ YukkiMusic/plugins/bot/mnhn.py
@@ -10,10 +10,10 @@ from pyrogram.types import ChatPermissions
 spam_chats = []
 
 @app.on_message(command(["منشن","@all","نادي الكل"]))
-async def mentionall(client, message):
+async def mention_all(client, message):
     chat_id = message.chat.id
     if message.chat.type == ChatType.PRIVATE:
-        return await message.reply("-› هذا الأمر فقط في المجموعات .")
+        return await message.reply("-› هذا الأمر فقط في المجموعات.")
 
     is_admin = False
     try:
@@ -33,43 +33,36 @@ async def mentionall(client, message):
         return await message.reply("-› اكتب منشن والرسالة التي تريد منشها \n-› مثلاً منشن صباح الخير .\n-› ولأيقاف المنشن اكتب ستوب .")
     elif message.text:
         mode = "text_on_cmd"
-        msg = message.text
+        msg_text = message.text.split(" ", 1)[1]
     elif message.reply_to_message:
         mode = "text_on_reply"
         msg = message.reply_to_message
         if not msg:
             return await message.reply("-› اكتب منشن والرسالة التي تريد منشها \n-› مثلاً منشن صباح الخير .\n-› ولأيقاف المنشن اكتب ستوب .")
+        msg_text = msg.text
     else:
         return await message.reply("-› اكتب منشن والرسالة التي تريد منشها \n-› مثلاً منشن صباح الخير .\n-› ولأيقاف المنشن اكتب ستوب .")
 
     spam_chats.append(chat_id)
-    usrnum = 0
-    usrtxt = ""
-    async for usr in client.get_chat_members(chat_id):
+    usr_list = []
+    async for usr in client.iter_chat_members(chat_id):
         if not chat_id in spam_chats:
             break
         if usr.user.is_bot:
             continue
-        usrnum += 1
-        usrtxt += f"[{usr.user.first_name}](tg://user?id={usr.user.id}) "
-
-        if usrnum == 1:
+        usr_list.append(usr.user.id)
+        if len(usr_list) >= 10:
             if mode == "text_on_cmd":
-                txt = f"{random.choice(usrtxt)}\n\n"
-                await client.send_message(chat_id, txt)
-                await asyncio.sleep(1)
-                usrnum = 0
-                usrtxt = ""
+                await client.send_message(chat_id, msg_text, reply_to_message_id=message.message_id, parse_mode="markdown")
             elif mode == "text_on_reply":
-                await msg.reply(f"{usrtxt}")
-                await asyncio.sleep(1)
-                usrnum = 0
-                usrtxt = ""
+                await msg.reply(msg_text, parse_mode="markdown")
+            usr_list = []
+            await asyncio.sleep(1)
+
     try:
         spam_chats.remove(chat_id)
     except:
         pass
-
 
 @app.on_message(command(["منشن ايقاف", "ستوب"]))
 async def cancel_spam(client, message):
