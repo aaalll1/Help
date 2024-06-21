e7e84d4917935788e8f5e322428aa5ff730b79f5
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-06-22 02:39:26 +0300

    Update purge.py

--- YukkiMusic/plugins/tools/purge.py
+++ YukkiMusic/plugins/tools/purge.py
@@ -16,16 +16,16 @@ async def purge_messages(event):
         not await user_is_admin(user_id=event.sender_id, message=event)
         and event.from_id not in SUDOERS
     ):
-        await event.reply("Only Admins are allowed to use this command")
+        await event.reply("-› الأمر للمشرفين فقط .")
         return
 
     if not await can_delete_messages(message=event):
-        await event.reply("Can't seem to purge the message")
+        await event.reply("-› حدث خطا .")
         return
 
     reply_msg = await event.get_reply_message()
     if not reply_msg:
-        await event.reply("Reply to a message to select where to start purging from.")
+        await event.reply("-› رد على الرسالة التي تريد بدء الحذف منها .")
         return
     messages = []
     message_id = reply_msg.id
@@ -43,7 +43,7 @@ async def purge_messages(event):
     except:
         pass
     time_ = time.perf_counter() - start
-    text = f"`Purged Successfully in {time_:0.2f} Seconds.`"
+    text = f"-› تم الحذف خلال {time_:0.2f} ثانية ."
     await event.respond(text, parse_mode="markdown")
 
 
@@ -55,16 +55,16 @@ async def delete_messages(event):
         not await user_is_admin(user_id=event.sender_id, message=event)
         and event.from_id not in SUDOERS
     ):
-        await event.reply("Only Admins are allowed to use this command")
+        await event.reply("-› الأمر للمشرفين فقط .")
         return
 
     if not await can_delete_messages(message=event):
-        await event.reply("Can't seem to delete this?")
+        await event.reply("-› حدث خطا .")
         return
 
     message = await event.get_reply_message()
     if not message:
-        await event.reply("Whadya want to delete?")
+        await event.reply("-› ماذا تريد حذفة ؟ ")
         return
     chat = await event.get_input_chat()
     del_message = [message, event.message]
@@ -76,7 +76,7 @@ async def delete_messages(event):
  ❍ /purge*:* deletes all messages between this and the replied to message.
 """
 
-PURGE_HANDLER = purge_messages, events.NewMessage(pattern="^[!/]purge$")
+PURGE_HANDLER = purge_messages, events.NewMessage(pattern="احذف منا")
 DEL_HANDLER = delete_messages, events.NewMessage(pattern="^[!/]del$")
 
 telethn.add_event_handler(*PURGE_HANDLER)
