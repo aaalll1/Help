3f5be50879adbfefd1cba5569c8d2fc4e035af75
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-06-22 01:46:20 +0300

    Update active.py

--- YukkiMusic/plugins/tools/active.py
+++ YukkiMusic/plugins/tools/active.py
@@ -1,26 +1,11 @@
-#
-# Copyright (C) 2024-present by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 from pyrogram import filters
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from YukkiMusic import app
 from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database.memorydatabase import (
-    get_active_chats,
-    get_active_video_chats,
-)
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from config import SUPPORT_CHANNEL  
-from strings.filters import command
+from YukkiMusic.utils.database.memorydatabase import get_active_chats, get_active_video_chats
+from config import SUPPORT_CHANNEL
 
-@app.on_message(command(["⦗ الاتصالات النشطة  ⦘", "الاتصالات النشطة"]))
+@app.on_message(filters.command(["⦗ الاتصالات النشطة  ⦘", "الاتصالات النشطة"]))
 async def active_chats(_, message: Message):
     if message.from_user.id not in SUDOERS:
         return await message.reply_text(
@@ -36,9 +21,8 @@ async def active_chats(_, message: Message):
         ]
     )
 
-    # إرسال الرد مع الزر الشفاف
+    # تعديل التنسيق ليكون على سطر واحد بالكامل بدون فواصل غير ضرورية
     await message.reply_text(
-        f"-› عزيزي المطور هذا هي اتصالات المجموعات التي يجرى تشغيل حساب المساعد فيها : \n\n
--› الصوت {ac_audio}\n-› الفيديو : {ac_video}",
+        f"-› عزيزي المطور هذه هي المجموعات الصوتية والفيديو التي يتم تشغيل المساعد فيها:\n\n-› الصوت: {ac_audio}\n-› الفيديو: {ac_video}",
         reply_markup=keyboard,
     )
