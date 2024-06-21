888b64a8e0831825bbc3afa5c5cbbb979ce9b6ee
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-06-22 01:04:56 +0300

    Update advice.py

--- YukkiMusic/plugins/tools/advice.py
+++ YukkiMusic/plugins/tools/advice.py
@@ -1,30 +1,26 @@
 from pyrogram import filters
-
+from strings.filters import command
 from config import LOG_GROUP_ID
 from YukkiMusic import api, app
 
 
-@app.on_message(filters.command("advice"))
+@app.on_message(command("advice"))
 async def advice(_, message):
     A = await message.reply_text("...")
     res = await api.advice()
     await A.edit(b["advice"])
 
 
-@app.on_message(filters.command("astronomical"))
+@app.on_message(command("احداث فلكية"))
 async def advice(_, message):
     a = await api.astronomy()
     if a["success"]:
         c = a["date"]
         url = a["imageUrl"]
         b = a["explanation"]
-        caption = f"Tᴏᴅᴀʏ's [{c}] ᴀsᴛʀᴏɴᴏᴍɪᴄᴀʟ ᴇᴠᴇɴᴛ:\n\n{b}"
+        caption = f"-› بتاريخ [{c}] مثل هذا اليوم حدث هذا :\n\n{b}"
         await message.reply_photo(url, caption=caption)
     else:
         await message.reply_photo("ᴛʀʏ ᴀғᴛᴇʀ sᴏᴍᴇ ᴛɪᴍᴇ")
-        await app.send_message(LOG_GROUP_ID, "/astronomical not working")
+        await app.send_message(LOG_GROUP_ID, "الامر لايعمل")
 
-
-__MODULE__ = "Cᴏɴᴛᴇɴᴛ's"
-__HELP__ = """
-/astronomical - ᴛᴏ ɢᴇᴛ ᴛᴏᴅᴀʏ's ᴀsᴛʀᴏɴᴏᴍɪᴄᴀʟ  ғᴀᴄᴛ"""
