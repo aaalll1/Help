3c71194d518544fb34cff2784e2e44ae7aab90eb
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-06-22 01:55:05 +0300

    Update active.py

--- YukkiMusic/plugins/tools/active.py
+++ YukkiMusic/plugins/tools/active.py
@@ -4,8 +4,9 @@ from YukkiMusic import app
 from YukkiMusic.misc import SUDOERS
 from YukkiMusic.utils.database.memorydatabase import get_active_chats, get_active_video_chats
 from config import SUPPORT_CHANNEL
+from strings.filters import command
 
-@app.on_message(filters.command(["⦗ الاتصالات النشطة  ⦘", "الاتصالات النشطة"]))
+@app.on_message(command(["⦗ الاتصالات النشطة  ⦘", "الاتصالات النشطة"]))
 async def active_chats(_, message: Message):
     if message.from_user.id not in SUDOERS:
         return await message.reply_text(
