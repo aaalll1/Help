18799fafa00d51c890cf36bca305c594df5989a1
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-06-22 01:09:46 +0300

    Update bard.py

--- YukkiMusic/plugins/tools/bard.py
+++ YukkiMusic/plugins/tools/bard.py
@@ -1,14 +1,14 @@
 import requests
 from pyrogram import filters
-
+from strings.filters import command
 from YukkiMusic import api, app
 
 
-@app.on_message(filters.command(["bard"]))
+@app.on_message(command(["فريدوم"]))
 async def bard(bot, message):
     if len(message.command) < 2 and not message.reply_to_message:
         await message.reply_text(
-            "Example:\n\n`/bard tell me about lord rama and sita in brief `"
+            "-› اكتب فريدوم واي شي تريد تسالة راح يجاوبك ."
         )
         return
 
