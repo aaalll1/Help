e3aef0af66f9f6bf9eead62addb77026b27bde21
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-06-28 15:21:46 +0300

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -1,33 +1,23 @@
-#
-# Copyright (C) 2024-present by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-
 from youtubesearchpython.__future__ import VideosSearch
-
+from config import YOUTUBE_IMG_URL  # استيراد المتغير YOUTUBE_IMG_URL من ملف config.py
 
 async def gen_thumb(videoid):
     try:
-        query = f"https://www.youtube.com/watch?v={videoid}"
-        results = VideosSearch(query, limit=1)
-        for result in (await results.next())["result"]:
-            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-        return thumbnail
+        # في هذا المثال، سنعيد YOUTUBE_IMG_URL مباشرة بدلاً من البحث عن صورة الفيديو
+        return YOUTUBE_IMG_URL
     except Exception as e:
-        return config.YOUTUBE_IMG_URL
-
+        return YOUTUBE_IMG_URL  # في حالة الخطأ، يتم إعادة YOUTUBE_IMG_URL
 
 async def gen_qthumb(vidid):
     try:
-        # Search for the video using video ID
-        query = f"https://www.youtube.com/watch?v={vidid}"
-        results = VideosSearch(query, limit=1)
-        for result in (await results.next())["result"]:
-            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-        return thumbnail
+        # في هذا المثال، سنعيد YOUTUBE_IMG_URL مباشرة بدلاً من البحث عن صورة الفيديو
+        return YOUTUBE_IMG_URL
     except Exception as e:
-        return config.YOUTUBE_IMG_URL
+        return YOUTUBE_IMG_URL  # في حالة الخطأ، يتم إعادة YOUTUBE_IMG_URL
+
+async def get_thumb_url(videoid):
+    try:
+        # في هذا المثال، سنعيد YOUTUBE_IMG_URL مباشرة بدلاً من البحث عن صورة الفيديو
+        return YOUTUBE_IMG_URL
+    except Exception as e:
+        return YOUTUBE_IMG_URL  # في حالة الخطأ، يتم إعادة YOUTUBE_IMG_URL
